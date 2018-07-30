85e8b641566b01f0d42b98a5b7c638c3777cc4d7
Jan Hybš <jan.hybs@tul.cz>
2018-07-30 12:11:00 +0200

    Decrease array size

--- benchmarks/main.cc
+++ benchmarks/main.cc
@@ -596,7 +596,7 @@ int main(int argc,  char* argv[]) {
     int sizes_l2[] = { 4 * KB, 8 * KB, 16 * KB, 32 * KB, 64 * KB, 128 * KB };
     if(testAll || test == t++) {
       tt.start();
-      test_mem(detailed["mem_l2"], sizes_l2, ARR_SIZE, 40*2);
+      test_mem(detailed["mem_l2"], sizes_l2, ARR_SIZE, 36*2);
       tt.stop();
       tmp["duration"] = tt.seconds;
       tmp["name"] = "mem_l2";
